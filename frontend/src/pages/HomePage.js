0f2be7bc2ac3f99bd36909da2d74ddbd6bfbc573
Jyri Ahia <jykke.ahia@gmail.com>
2023-05-26 14:56:45 +0300

    sivut vielä mukaan

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* Add your content here */}
+      <p>This is a weird search system for a coding demo. It mixes different things in weird way in order to show know how. It has no other purpose.</p>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ frontend/src/pages/SearchPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DynamicFormComponent from '../components/dynamicSearchForm'
+
+const SearchPage = () => {
+  return (
+    <div>
+      <h1>Search Page</h1>
+      <DynamicFormComponent />
+    </div>
+  );
+};
+
+export default SearchPage;
