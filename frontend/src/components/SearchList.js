199498fd98cb79a0addefdfc09d3debe4cc530e9
Jyri Ahia <jykke.ahia@gmail.com>
2023-05-28 15:39:43 +0300

    etenee

--- frontend/src/components/SearchList.js
+++ frontend/src/components/SearchList.js
@@ -3,7 +3,92 @@ import axios from 'axios';
 
 const SearchListComponent = () => {
     const [searches, setSearches] = useState({});
-    const [options, setOptions] = useState({}); // TODO tää tieto pitäisi olla Contextissa tai muussa globaalissa tilassa tms. 
+    const [options, setOptions] = useState({
+        searchcarprice: {
+            id: {
+              type: 'integer',
+              required: false,
+              read_only: true,
+              label: 'ID'
+            },
+            title: {
+              type: 'string',
+              required: true,
+              read_only: false,
+              label: 'Title',
+              max_length: 120
+            },
+            create_datetime: {
+              type: 'datetime',
+              required: false,
+              read_only: true,
+              label: 'Create datetime'
+            },
+            script: {
+              type: 'string',
+              required: true,
+              read_only: false,
+              label: 'Script',
+              max_length: 24
+            },
+            parameter_model: {
+              type: 'string',
+              required: true,
+              read_only: false,
+              label: 'Parameter model',
+              max_length: 256
+            },
+            parameter_make: {
+              type: 'string',
+              required: true,
+              read_only: false,
+              label: 'Parameter make',
+              max_length: 256
+            }
+          },
+          searchwebshop: {
+            id: {
+              type: 'integer',
+              required: false,
+              read_only: true,
+              label: 'ID'
+            },
+            title: {
+              type: 'string',
+              required: true,
+              read_only: false,
+              label: 'Title',
+              max_length: 120
+            },
+            create_datetime: {
+              type: 'datetime',
+              required: false,
+              read_only: true,
+              label: 'Create datetime'
+            },
+            script: {
+              type: 'string',
+              required: true,
+              read_only: false,
+              label: 'Script',
+              max_length: 24
+            },
+            search_string: {
+              type: 'string',
+              required: true,
+              read_only: false,
+              label: 'Search string',
+              max_length: 256
+            },
+            search_max_price: {
+              type: 'integer',
+              required: true,
+              read_only: false,
+              label: 'Search max price'
+            }
+          }
+    }); // TODO tää tieto pitäisi olla Contextissa tai muussa globaalissa tilassa tms. 
+    // TODO lisäksi ei lataudu tarpeeksi ajoissa eli pitäisi varmaan tehdä alussa myös tämän haku
 
     useEffect(() => {
         fetchSearches();
@@ -48,22 +133,28 @@ const SearchListComponent = () => {
     return (
         <>
            <h2>Searches</h2>
-           {searches.searches ? (
+           {searches.searches && options ? (
                 <div>
                     {Object.keys(searches.searches).map((key, index) => {
                         return (
                             <div>
-                                <h3>{key} - {index}</h3>
-                                {JSON.stringify(searches.searches[key].saved_searches)}
+                                <h3 key={index}>{key}</h3>
+                                { JSON.stringify(searches.searches[key].saved_searches) }
                                 
                                 {Object.values(searches.searches[key].saved_searches).map(search => {
                                     return (
                                         <div>
                                             <ul>
-                                                <li>Search ID : {search.id}</li>
-                                                <li>Search title: {search.title}</li>
-                                                <li>Search date: {search.create_datetime}</li>
-                                                <li></li>
+                                                <li key={search.id}>Search ID : {search.id}</li>
+                                                <li key={search.script}>Script: {search.script}</li>
+                                                {Object.keys(options[key]).map((searchParameter, index2) => {
+                                                    if(searchParameter !== 'id' && searchParameter !== 'script') {
+                                                        return (
+                                                          <li key={searchParameter+"_"+index2}>{Object.values(options[key][searchParameter].label)} : {search[searchParameter]}</li>
+                                                        )
+                                                    }
+                                                    return ( <></> )
+                                                })}
                                             </ul>
                                         </div>
                                     )
@@ -74,6 +165,11 @@ const SearchListComponent = () => {
 
                     <p>JSON auki</p>
                     {JSON.stringify(options)}
+                    {Object.values(options).map((option) => {
+                        return (
+                            <div>{option.id.type}</div>
+                        )
+                    })}
                 </div>
             ) : (
                 <p>No searches</p>   
